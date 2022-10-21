c7ce40d097a762a6d356f0da7cb847ce736f077e
Kobe Conomon <kobeconomon@knights.ucf.edu>
2022-10-21 17:22:56 -0400

    updated ESP MQTT file

--- ReadCard_MQTT_Edit_Kobe/ReadCard_MQTT_Edit_Kobe.ino
+++ ReadCard_MQTT_Edit_Kobe/ReadCard_MQTT_Edit_Kobe.ino
@@ -1,19 +1,31 @@
 // Kobe Conomon
 // Zakaria Antifit
-// Last modified: 5/31/2022
-// Program that reads data from a PICC (an RFID tag or card) using a MFRC522-based RFID reader on the Arduino SPI interface
+// Last modified: 10/21/2022
+// Program that reads data from a HC-SR04 Ultrasonic Sensor to an ESP 32, and sends it to an MQTT broker (in this case, a 
+// Raspberry Pi 4 running Ubuntu Server OS)
 
-#include <SPI.h>
-#include <MFRC522.h>
+#include <String.h>
+
+#include <Ultrasonic.h>
 #include <PubSubClient.h> // Connect and publish to the MQTT broker
 #include <WiFi.h> // Enables the ESP32 to connect to the local network (via WiFi)
 
 // WiFi variables
-const char* ssid = "CenturyLink9364";
-const char* wifi_password = "a7k3fdaaef8caa";
+// const char* ssid = "CenturyLink9364";
+// const char* wifi_password = "a7k3fdaaef8caa";
+
+// const char* ssid = "UCF_WPA2";
+// const char* wifi_password = "BaSEbAll.,/251111";
+
+// const char* ssid = "UCF_Guest";
+// const char* wifi_password;
+
+// We might need to use my mobile hotspot if UCF wifi is going to be finnicky (spoiler: it will be)
+const char* ssid = "Hacker";
+const char* wifi_password = "yEa b011";
 
 // MQTT variables
-const char* broker_IP = "192.168.0.50";
+const char* broker_IP = "192.168.0.51";
 const char* RFID_topic = "home/bedroom/id";
 const char* MQTT_username = "kconomon";
 const char* MQTT_passwd = "Baseball.,/251";
@@ -23,16 +35,16 @@ const char* client_ID = "client_bedroom";
 int wifi_flag = 0;
 int MQTT_flag = 0;
 
+// trigger and echo pins - read data from sensor to ESP (will need a lot more of these)
+int trig = 15; // ESP32 pin GIOP15
+int echo = 2; // ESP32 pin GIOP2
+
+int distTrigger = 200;
+
 WiFiClient wifiClient; // Creates client object that can connect to a specified IP and port
 PubSubClient client(broker_IP, 1883, wifiClient); // Creates client object that allows you to use MQTT
 
-//SPI Setup
-#define SS_PIN  5  // ESP32 pin GIOP5  (SDA)
-#define RST_PIN 27 // ESP32 pin GIOP27 (RST)
-
-String uid;
-
-MFRC522 rfid(SS_PIN, RST_PIN);
+Ultrasonic ultrasonic(trig, echo); // ultrasonic sensor object
 
 // Function taken from article: https://diyi0t.com/microcontroller-to-raspberry-pi-wifi-mqtt-communication/
 // Custom function to connet to the MQTT broker via WiFi
@@ -67,9 +79,8 @@ void connect_MQTT(){
 }
 
 void setup() {
-  Serial.begin(9600); // ESP32 baud rate
-  SPI.begin(); // initialize SPI bus
-  rfid.PCD_Init(); // initialize MFRC522
+  int distance;
+  Serial.begin(115200); // ESP32 baud rate
 }
 
 void loop() {
@@ -79,28 +90,22 @@ void loop() {
     connect_MQTT();
     Serial.setTimeout(10000);
   }
-  
-  if (rfid.PICC_IsNewCardPresent()) 
-  { 
-    // new tag is available
-    if (rfid.PICC_ReadCardSerial()) 
-    { 
-      Serial.println(uid);
-      convertCardID(rfid.uid.uidByte, rfid.uid.size); // function to read card UID
-      
-      Serial.println("UID: " + uid); // prints uid with newline
-      rfid.PICC_HaltA(); // halt PICC
 
-      PublishID();
-    }
+  distance = ultrasonic.read();
+
+  if (distance < distTrigger)
+  {
+    Serial.println(distance);
+    String(distance)
+    PublishSensorInfo();
   }
+  delay(10);
 }
 
-// Send data from the publisher to the broker
-void PublishID()
+void PublishSensorInfo()
 {
   // PUBLISH to the MQTT Broker 
-  if (client.publish(RFID_topic, uid.c_str())) {
+  if (client.publish(RFID_topic, distance)) {
     Serial.println("ID sent!");
   }
   // Again, client.publish will return a boolean value depending on whether it succeded or not.
@@ -110,16 +115,8 @@ void PublishID()
     if(client.connect(client_ID, MQTT_username, MQTT_passwd)) 
       Serial.println("Reconnected");
     delay(10); // This delay ensures that client.publish doesn't clash with the client.connect call
-    client.publish(RFID_topic, uid.c_str());
+    client.publish(RFID_topic, distance);
   }
   // client.disconnect();  // disconnect from the MQTT broker
   delay(1000*2);       // Ready for new card every 2 seconds
-}
-
-// Converts uid from hex to a string
-void convertCardID(byte * buffer, byte bufferSize)
-{
-  uid = ""; // increment uid with buffer
-  for (int i = 0; i < bufferSize; i++) 
-    uid = uid + String(buffer[i], HEX);
 }
