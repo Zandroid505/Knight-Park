5f90b0f893cced956e94d06801bed74302f608eb
KobeConomon <kobeconomon@knights.ucf.edu>
2022-11-15 22:11:19 -0500

    updated parkSpotFetch run continuously

--- Website/parkSpotFetch.py
+++ Website/parkSpotFetch.py
@@ -3,9 +3,12 @@ import sys
 
 
 # must match port, baudrate, and delay() on Arduino file
-esp32 = serial.Serial('COM4', baudrate=115200, timeout=2.5)
-print(esp32.readline().decode('UTF-8'))
-sys.stdout.flush()
+esp32 = serial.Serial('COM5', baudrate=115200, timeout=2.5)
+
+while 1:
+    data = esp32.readline().decode('UTF-8')
+    print(data)
+    # sys.stdout.flush()
 
 
 # def parse_parking_info(spot):
